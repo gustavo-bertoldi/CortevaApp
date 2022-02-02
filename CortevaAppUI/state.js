83a56b8f5a4da14034c766237420f3f574bf00b8
Gustavo Bertoldi <gg.bertoldi@hotmail.com>
2022-02-02 14:02:40 +0100

    UI

---
+++ CortevaAppUI/state.js
@@ -0,0 +1,35 @@
+let state = {
+
+    user: [],
+    pos: [],
+    sites: [],
+    events1: [],
+    events2: [],
+    unplannedEvent_UnplannedDowntime :[],
+    downtimeReasons: [],
+    downtimeReasons_2 : [],
+    machineIssue : [],
+    unplannedEvent_ChangingFormat: [],
+    unplannedEvent_ClientChanging: [],
+    unplannedEvent_CIP: [],
+    plannedEvent: [],
+    speedLoss : [],
+    assignation : [],
+    PO : [],
+    netOP: [],
+    machines : [],
+    allEvents : [],
+    volumes: [],
+    worksiteID: -1,
+    productionlineID: -1,
+    token: localStorage.getItem('access_token'),
+    unplannedDowntimeEvents: [],
+    checkPO : [],
+    REJECTION: [],
+    unplannedDowntimeMachineShutdownTypes: [],
+    qualityLosses: [],
+    performanceIndexes: [],
+    getSpeedLosses: []
+};
+
+export default state
